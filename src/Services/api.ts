2c0e672f2931ea21d98cfec10e40d5ae7623bff1
Leonardo Amato <leo.amato@hotmail.com>
2022-08-29 16:45:55 +0200

    services directory name changed

---
+++ src/Services/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const movieDB = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/movie',
+  params: {
+    api_key: 'a3750cf023828aeba1f8573bab0f27e9',
+    language: 'en-US',
+  },
+});
+
+export default movieDB;
